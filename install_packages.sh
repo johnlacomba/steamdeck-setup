dacf91138213c6bb3ba9f8ea1d54a312b107974e
John <johnlacomba@gmail.com>
2022-07-27 14:23:05 -0400

    Updated install_packages.sh to install pip.  Many comments have been switched to echo statements.  The path to pip is also now appended to /etc/profile if necessary.

--- install_packages.sh
+++ install_packages.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
 apptoinstallList="vi glances"
 
-# Disable file system being read only
+echo "--------------------------------------" 
+echo "Disabling the read-only lock on the file system."
+echo "--------------------------------------" 
 sudo steamos-readonly disable
 # Alternative method to disable the read only limit
 #sudo btrfs property set -ts / ro false
-# Initialize the pacman keyring
+
+echo "--------------------------------------" 
+echo "Initializing the pacman keyring."
+echo "--------------------------------------" 
 sudo pacman-key --init
-# Populate the pacman keyring with default Arch Linux keys
+echo "--------------------------------------" 
+echo "Populate the pacman keyring with default Arch Linux keys"
+echo "--------------------------------------" 
 sudo pacman-key --populate archlinux
-# Install all packages from $apptoinstallList
-eval 'sudo pacman --noconfirm -S ${apptoinstallList}' 
+
+echo "--------------------------------------" 
+echo "Install all packages from \$apptoinstallList"
+echo "--------------------------------------" 
+eval 'sudo pacman --noconfirm -S ${apptoinstallList}'
+
+echo "--------------------------------------"
+echo "Downloading and installing pip."
+echo "--------------------------------------" 
+curl -O https://bootstrap.pypa.io/get-pip.py
+sudo -u deck python get-pip.py
+echo "--------------------------------------" 
+echo "Cleaning up pip setup script."
+echo "--------------------------------------" 
+rm get-pip.py
+
+# If the pip directory has not been added to $PATH then add it
+if [[ ! $(grep "append_path '/home/deck/.local/bin'" /etc/profile) ]]; then
+  echo "--------------------------------------" 
+  echo "Adding /home/deck/.local/bin to PATH in /etc/profile"
+  echo "--------------------------------------" 
+  sudo sed -i "/# Append our default paths/ a append_path '/home/deck/.local/bin'" /etc/profile
+fi
