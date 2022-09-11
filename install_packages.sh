afb831f16f04ce3843bdc82c31e4676f532bd87c
John <johnlacomba@gmail.com>
2022-09-11 13:01:53 -0400

    Updated install_packages.sh with Lutris and all supporting apps.  Added a permissions check.  Updated the runtime output and comments.

--- install_packages.sh
+++ install_packages.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-apptoinstallList="vi glances"
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+apptoinstallList="vi glances gnu-netcat nmap giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader dosbox"
 piptoinstallList="requests beautifulsoup4"
 
 echo "--------------------------------------" 
@@ -19,9 +25,24 @@ echo "--------------------------------------"
 sudo pacman-key --populate archlinux
 
 echo "--------------------------------------" 
-echo "Install all packages from \$apptoinstallList"
+echo "Install wine-staging and winetricks, then all packages from \$apptoinstallList, then cups and samba"
 echo "--------------------------------------" 
+eval 'sudo pacman --noconfirm -S wine-staging winetricks'
 eval 'sudo pacman --noconfirm -S ${apptoinstallList}'
+echo "--------------------------------------"
+echo "Note: Cups and Samba are installed with the pacman parameter '--overwrite'" 
+echo "--------------------------------------" 
+eval 'sudo pacman --noconfirm -S --overwrite "*" cups samba'
+
+echo "--------------------------------------" 
+echo "Download and install Lutris"
+echo "--------------------------------------" 
+wget -O lutris_install https://archlinux.org/packages/community/any/lutris/download
+echo "sudo pacman -U lutris_install"
+echo "--------------------------------------" 
+echo "Cleaning up lutris_install file."
+echo "--------------------------------------" 
+rm ./lutris_install
 
 echo "--------------------------------------"
 echo "Downloading and installing pip."
@@ -43,5 +64,5 @@ fi
 
 echo "--------------------------------------"
 echo "Installing pip packages from \$piptoinstallList"
-echo "--------------------------------------"
+echo "--------------------------------------" 
 eval 'sudo -u deck pip install --no-input ${piptoinstallList}'
